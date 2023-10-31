c56ddd3137193e20b257ce7411e250e51e19f1ca
pdLeal <lealpd@outlook.com>
2023-10-31 11:07:31 -0300

    add: comments and refectoring

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Seleciona as setas
-const aclamados = document.querySelectorAll(".aclamados");
-aclamados.forEach(sec => {
+const filmes = document.querySelectorAll(".filmes");
+filmes.forEach(sec => {
     const setaEsquerda = sec.firstElementChild;
     const setaDireita = sec.lastElementChild;
     setaDireita.addEventListener("click", scrollRight);
@@ -8,36 +8,37 @@ aclamados.forEach(sec => {
 });
 // Seleciona os outer-containers e seus respectivos tamanhos
 const outers = document.querySelectorAll(".outer-container");
-let outerCima = outers[0];
-let outerBaixo = outers[1];
+const outerCima = outers[0];
+const outerBaixo = outers[1];
 let cimaWidth = outerCima.offsetWidth;
 let baixoWidth = outerBaixo.offsetWidth;
 // Difine a posição inicial das divs escondidas
-if (window.innerWidth > 789) {
+if (window.innerWidth >= 790) {
     outerCima.lastElementChild.style.transform = `translate(${cimaWidth + 16}px)`;
     outerBaixo.lastElementChild.style.transform = `translate(${baixoWidth + 16}px)`;
 }
 
 function scrollRight() {
     ajustarOuter();
+    
     const previousSibling = this.previousElementSibling;
-    previousSibling.firstElementChild.classList.add("hidden-left");
-    previousSibling.lastElementChild.classList.remove("hidden-right");
+    previousSibling.firstElementChild.classList.add("hidden");
+    previousSibling.lastElementChild.classList.remove("hidden");
+    previousSibling.lastElementChild.style.transform = `translate(0)`;
 
-    if (outerCima === previousSibling) {
+    if (outerCima === previousSibling) { // Caso outerCima e outerBaixo possuam tamanhos diferentes, garante q o container se mova de acordo com o tamanho correto 
         previousSibling.firstElementChild.style.transform = `translate(-${cimaWidth + 16}px)`;
     } else {
         previousSibling.firstElementChild.style.transform = `translate(-${baixoWidth + 16}px)`;
     }
-
-    previousSibling.lastElementChild.style.transform = `translate(0)`;
 }
 
 function scrollLeft() {
     ajustarOuter();
+
     const nextSibling = this.nextElementSibling;
-    nextSibling.firstElementChild.classList.remove("hidden-left");
-    nextSibling.lastElementChild.classList.add("hidden-right");
+    nextSibling.firstElementChild.classList.remove("hidden");
+    nextSibling.lastElementChild.classList.add("hidden");
     nextSibling.firstElementChild.style.transform = `translate(0)`;
 
     if (outerCima === nextSibling) {
@@ -47,31 +48,24 @@ function scrollLeft() {
     }
 }
 
-function ajustarOuter() {
+function ajustarOuter() { // Caso a página seja redimensionada, garante q os outer-containers ñ se movam mais, ou menos, q o ncessário 
     cimaWidth = outerCima.offsetWidth;
     baixoWidth = outerBaixo.offsetWidth;
 }
 
-function resizeToSmallWindow() {
-    if (window.innerWidth <= 789) {
+function resizeWindow() {
+    if (window.innerWidth < 790) {
         for (let i = 0; i < outers.length; i++) {
-            outers[i].lastElementChild.classList.remove("hidden-right");
-            outers[i].firstElementChild.classList.remove("hidden-right");
-
-            outers[i].lastElementChild.classList.remove("hidden-left");
-            outers[i].firstElementChild.classList.remove("hidden-left");
+            outers[i].lastElementChild.classList.remove("hidden");
+            outers[i].firstElementChild.classList.remove("hidden");
 
             outers[i].lastElementChild.style.transform = `translate(0)`;
             outers[i].firstElementChild.style.transform = `translate(0)`;
         }
-    }
-}
-
-function resizeToBigWindow() {
-    if (window.innerWidth > 789) {
+    } else {
         for (let i = 0; i < outers.length; i++) {
-            outers[i].lastElementChild.classList.add("hidden-right");
-            outers[i].firstElementChild.classList.remove("hidden-left");
+            outers[i].lastElementChild.classList.add("hidden");
+            outers[i].firstElementChild.classList.remove("hidden");
 
             outers[i].lastElementChild.style.transform = `translate(${outers[i].offsetWidth + 16}px)`;
             outers[i].firstElementChild.style.transform = `translate(0)`;
@@ -79,5 +73,5 @@ function resizeToBigWindow() {
     }
 }
 
-window.addEventListener("resize", resizeToSmallWindow);
-window.addEventListener("resize", resizeToBigWindow);
+
+window.addEventListener("resize", resizeWindow);
